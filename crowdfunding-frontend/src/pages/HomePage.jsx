f4c46cd5ccf51c5406a5886d52f73ab85ae5b591
Inano Knowles <inano_knowles@27-33-216-194.tpgi.com.au>
2026-02-07 15:24:24 +1000

    crowdfunding_frontend_setup

---
+++ crowdfunding-frontend/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import FundraiserCard from "../components/FundraiserCard";
+import "./HomePage.css";
+
+function HomePage() {
+    return (
+        <div id="fundraiser-list">
+            {allFundraisers.map((fundraiserData, key) => {
+                return <FundraiserCard key={key} fundraiserData={fundraiserData} />;
+            })}
+        </div>
+    );
+}
+
+export default HomePage;
